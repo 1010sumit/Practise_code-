d6830fbbde67e894b87302d197454fdd3fe0313a
1010sumit <sumitprajapati1010@gmail.com>
2024-10-02 22:11:29 +0530

    commit1

---
+++ src/main/java/org/example/Generics/method_example.java
@@ -0,0 +1,19 @@
+package org.example.Generics;
+
+public class method_example {
+    public <T> void printArray(T[] array){
+        for(T element : array){
+            System.out.print(element + " ");
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] array = {1,2,34,4,5};
+        String[] StringArray = {"sumit", "sujal ", "anand"};
+        method_example m = new method_example();
+        m.printArray(array);
+        System.out.println();
+        m.printArray(StringArray);
+    }
+}
